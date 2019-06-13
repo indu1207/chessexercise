c2f2a95fe08dfda9ef36b962e32292a044a13841
root <root@ip-172-31-88-133.ec2.internal>
2019-06-13 18:04:13 +0000

    Updated chessexcer file

--- chessexcer.py
+++ chessexcer.py
@@ -1,44 +1,48 @@
 #!/usr/bin/python3
 
 import chess
-print ("Taking values ")
-def enumerate_moves(x, y, type):
-
+print ("Evaluating potential chess board positions ")
+def evaluate_moves(position, type):
+    pos = str(position)
+    x = int(pos[0])
+    y = int(pos[1])
     possible_moves = []     # Intializing empty list
     print ("Position is: ",( x , y))
     print("Piece type is :", type)
     board = chess.Board()
     # resolving knight moves. 
     if type  == "Knight":
-        print("Possible moves are: ")
+        print("Possible Knight moves are: ")
         possible_moves += [(x+2, y+1), (x+2, y-1)
                            ,(x+1, y+2), (x+1,y-2)
                            ,(x-2, y-1), (x-2, y+1)
                            ,(x-1, y+2), (x-1, y-2)]
         print(possible_moves)
-
+    
+    #Resolving Rook moves
     elif type == "Rook":
-        # resolving down side rook
+        print("Possible Rook moves are: ")
+        # resolving down side rook moves
         a = x + 1
         b = y
         while (7 > a >= 0 and 7 > b >= 0) and (board[a][b] == ""):
-            possible_moves.append([[a], [b]])
+            possible_moves += ([[a], [b]])
             try:
                 if board[a+1][b][0] == "w":
-                    possible_moves.append([[a+1], [b]])
+                    possible_moves += ([[a+1], [b]])
             except IndexError:
                 pass
             a += 1
 
-        # resolving left side
+        # resolving left side rook moves
         a = x
         b = y - 1
         while (7 > a >= 0 and 7 > b >= 0) and (board[a][b] == ""):
-            possible_moves.append([[a], [b]])
+            possible_moves += ([[a], [b]])
             try:
                 if board[a][b-1][0] == "w":
-                    possible_moves.append([[a], [b-1]])
-            except IndexError:
+                    possible_moves += ([[a], [b-1]])
+            except IndexError: 
                 pass
             b -= 1
 
@@ -46,10 +50,10 @@ def enumerate_moves(x, y, type):
         a = x - 1
         b = y
         while (7 > a >= 0 and 7 > b >= 0) and (board[a][b] == ""):
-            possible_moves.append([[a], [b]])
+            possible_moves += ([[a], [b]])
             try:
                 if board[a-1][b][0] == "w":
-                    possible_moves.append([[a-1], [b]])
+                    possible_moves += ([[a-1], [b]])
             except IndexError:
                 pass
             a -= 1
@@ -58,23 +62,25 @@ def enumerate_moves(x, y, type):
         a = x
         b = y + 1
         while (7 > a >= 0 and 7 > b >= 0) and (board[a][b] == ""):
-            possible_moves.append([[a], [b]])
+            possible_moves += ([[a], [b]])
             try:
                 if board[a][b+1][0] == "w":
-                    possible_moves.append([[a], [b+1]])
+                    possible_moves += ([[a], [b+1]])
             except IndexError:
                 pass
             b += 1    
-
+    
+    #Resolving Queen moves
     elif type  == "Queen":
+        print("Possible Queen moves are: ")
         # Resolving down and right side Queen
         a = x + 1
         b = y + 1
         while (7 > a >= 0 and 7 > b >= 0) and (board[a][b] == ""):
-            possible_moves.append([[a], [b]])
+            possible_moves += ([[a], [b]])
             try:
                 if board[a+1][b+1][0] == "w":
-                    possible_moves.append([[a+1], [b+1]])
+                    possible_moves += ([[a+1], [b+1]])
             except IndexError:
                 pass
             a += 1
@@ -84,23 +90,23 @@ def enumerate_moves(x, y, type):
         a = x + 1
         b = y - 1
         while (7 > a >= 0 and 7 > b >= 0) and (board[a][b] == ""):
-            possible_moves.append([[a], [b]])
+            possible_moves += ([[a], [b]])
             try:
                 if board[a+1][b-1][0] == "w":
-                    possible_moves.append([[a+1], [b-1]])
+                    possible_moves += ([[a+1], [b-1]])
             except IndexError:
                 pass
             a += 1
             b -= 1
 
-        # resolving up and right
+        # resolving up and right side
         a = x - 1
         b = y + 1
         while (7 > a >= 0 and 7 > b >= 0) and (board[a][b] == ""):
-            possible_moves.append([[a], [b]])
+            possible_moves += ([[a], [b]])
             try:
                 if board[a-1][b+1][0] == "w":
-                    possible_moves.append([[a-1], [b+1]])
+                    possible_moves += ([[a-1], [b+1]])
             except IndexError:
                 pass
             a -= 1
@@ -110,10 +116,10 @@ def enumerate_moves(x, y, type):
         a = x - 1
         b = y - 1
         while (7 > a >= 0 and 7 > b >= 0) and (board[a][b] == ""):
-            possible_moves.append([[a], [b]])
+            possible_moves += ([[a], [b]])
             try:
                 if board[a-1][b-1][0] == "w":
-                    possible_moves.append([[a-1], [b-1]])
+                    possible_moves += ([[a-1], [b-1]])
             except IndexError:
                 pass
             a -= 1
@@ -123,10 +129,10 @@ def enumerate_moves(x, y, type):
         a = x + 1
         b = y
         while (7 > a >= 0 and 7 > b >= 0) and (board[a][b] == ""):
-            possible_moves.append([[a], [b]])
+            possible_moves += ([[a], [b]])
             try:
                 if board[a+1][b][0] == "w":
-                    possible_moves.append([[a+1], [b]])
+                    possible_moves += ([[a+1], [b]])
             except IndexError:
                 pass
             a += 1
@@ -135,10 +141,10 @@ def enumerate_moves(x, y, type):
         a = x
         b = y - 1
         while (7 > a >= 0 and 7 > b >= 0) and (board[a][b] == ""):
-            possible_moves.append([[a], [b]])
+            possible_moves += ([[a], [b]])
             try:
                 if board[a][b-1][0] == "w":
-                    possible_moves.append([[a], [b-1]])
+                    possible_moves += ([[a], [b-1]])
             except IndexError:
                 pass
             b -= 1
@@ -147,10 +153,10 @@ def enumerate_moves(x, y, type):
         a = x - 1
         b = y
         while (7 > a >= 0 and 7 > b >= 0) and (board[a][b] == ""):
-            possible_moves.append([[a], [b]])
+            possible_moves += ([[a], [b]])
             try:
                 if board[a-1][b][0] == "w":
-                    possible_moves.append([[a-1], [b]])
+                    possible_moves += ([[a-1], [b]])
             except IndexError:
                 pass
             a -= 1
@@ -159,12 +165,12 @@ def enumerate_moves(x, y, type):
         a = x
         b = y + 1
         while (7 > a >= 0 and 7 > b >= 0) and (board[a][b] == ""):
-            possible_moves.append([[a], [b]])
+            possible_moves += ([[a], [b]])
             try:
                 if board[a][b+1][0] == "w":
-                    possible_moves.append([[a], [b+1]])
+                    possible_moves += ([[a], [b+1]])
             except IndexError:
                 pass
             b += 1
 
-enumerate_moves(2, 2, "Knight")
+evaluate_moves(22, "Knight")
